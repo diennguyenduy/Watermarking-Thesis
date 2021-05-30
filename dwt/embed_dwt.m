c400dffbc33016641db2980ca9dc757f20cca63d
Dien-nd <38159404+diennguyenduy@users.noreply.github.com>
2021-05-30 15:28:51 +0700

    fix sth

--- dwt/embed_dwt.m
+++ dwt/embed_dwt.m
@@ -1,46 +1,52 @@
-% Thực hiện nhúng thủy vân sử dụng thuật toán DWT
+%Nhúng thủy vân bằng thuật toán DWT
+%Đầu vào:
+%   - img: Ảnh gốc
+%   - watermark_img: Thủy vân sử dụng để nhúng
+%Đầu ra:
+%   - watermarked: Ảnh gốc sau khi nhúng thủy vân
+%   - time: Thời gian thực hiện
+
 function [watermarked, time] = embed_dwt(varargin)
 
-%Lấy ảnh gốc và biến đổi
-img = varargin{1};
-img = double(img);
-[p, q, r] = size(img);
+    %Lấy ảnh gốc từ đầu vào
+    img = varargin{1};
+    img = double(img);
+    [p, q, r] = size(img);
 
-%Lấy thủy vân và thực hiện biến đổi
-watermark_img = varargin{2};
-watermark = imresize(watermark_img,[p/2 q/2]);
-watermark = double(watermark);
+    %Lấy thủy vân từ đầu vào
+    watermark_img = varargin{2};
+    watermark = imresize(watermark_img,[p/2 q/2]);
+    watermark = double(watermark);
 
-weight = 0.01; %the weight of watermarking
-height = p/4;
-width  = q/4;
+    weight = 0.01; %Hệ số nhúng
+    height = p/4;
+    width  = q/4;
 
-tic;
-for k = 1:r
-    [ca,ch,cv,cd] = dwt2(img(:,:,k),'db1');  %Thực hiện biến đổi wavelet mức 1 cho ảnh gốc
-    [caa,cha,cva,cda] = dwt2(ca,'db1');      %Chọn dải tần phụ ca và thực hiện biến đổi wavelet mức 2 để thực hiện nhúng thủy vân vào dải này
-    y = [caa cha;cva cda];
-    %disp(size(y));    %[256 256]
-    Temp = y + weight*watermark(:,:,k);
-    %disp(size(Temp)); %[256 256]
-    for i = 1:height
-        for j = 1:width
-            ncaa(i,j) = Temp(i,j);
-            ncha(i,j) = Temp(i,j+width);
-            ncva(i,j) = Temp(i+height,j);
-            ncda(i,j) = Temp(i+height,j+height);
+    tic;
+    for channel = 1:r
+        [ca,ch,cv,cd] = dwt2(img(:,:,channel),'haar');  %Biến đổi wavelet mức 1 cho channel thứ r của ảnh gốc (R-G-B)
+        [ca2,ch2,cv2,cd2] = dwt2(ca,'haar');            %Biến đổi wavelet mức 2 cho dải tần phụ ca
+        y = [ca2 ch2;cv2 cd2];
+        %disp(size(y));    %[256 256] %Cùng kích thước với thủy vân
+        Watermarked = y + weight*watermark(:,:,channel);
+        for i = 1:height
+            for j = 1:width
+                marked_ca2(i,j) = Watermarked(i,j);
+                marked_ch2(i,j) = Watermarked(i,j+width);
+                marked_cv2(i,j) = Watermarked(i+height,j);
+                marked_cd2(i,j) = Watermarked(i+height,j+height);
+            end
         end
+        marked_ca = idwt2(marked_ca2,marked_ch2,marked_cv2,marked_cd2,'haar');
+        watermarked_img(:,:,channel) = (idwt2(marked_ca,ch,cv,cd,'haar'));
     end
-    d1 = idwt2(ncaa,ncha,ncva,ncda,'db1');
-    watermarked_img(:,:,k) = (idwt2(d1,ch,cv,cd,'db1'));
-end
-time = toc;
+    time = toc;
 
-watermarked = uint8(watermarked_img);
+    watermarked = uint8(watermarked_img);
 
-filter = {'*.fits';'*.xls';'*.mat';'*.*'};
-[file, path] = uiputfile(filter, 'Lưu file giá trị ảnh đã nhúng thủy vân');
-save_path = strcat(path,'/',file);
-fitswrite(watermarked_img, save_path);
+    filter = {'*.fits';'*.xls';'*.mat';'*.*'};
+    [file, path] = uiputfile(filter, 'Lưu file giá trị số thực của ảnh đã nhúng thủy vân');
+    save_path = strcat(path,'/',file);
+    fitswrite(watermarked_img, save_path);
 
 end
--- lsb/extract_lsb.m
+++ lsb/extract_lsb.m
@@ -10,24 +10,23 @@ function varargout = extract_lsb(varargin)
     watermark_size = fscanf(keyfile, '%d');
     fclose(keyfile);
 
-	len = prod(watermark_size);                 %Biến len có kích thước bằng với thủy vân đã nhúng + 4 bytes (chứa header)
+	len = prod(watermark_size);                 %Biến len có kích thước bằng với thủy vân đã nhúng
     im_log(prod(watermark_size)) = 0;           %Tạo mảng có kích thước bằng với thủy vân đã nhúng để lưu thủy vân
 
     k = 0;
-
     p = randperm(prod(watermark_size)*8);       %Sinh chuỗi ngẫu nhiên trùng với chuỗi của quá trình nhúng
 
     while k < len
         k = k+1;
         for j = 1:8
-            index = (k-1)*8 + j;	%Xét 8 pixel một lúc của ảnh chứa thủy vân (Để lấy ra 1 pixel của thủy vân)
-            b = bitget(watermarked_array(p(index)),1);  %Lấy ra bit ở vị trí thứ 1 (LSB) của pixel j của ảnh chứa thủy vân
+            index = (k-1)*8 + j;	%Xét 8 pixel một lúc của ảnh chứa để lấy ra 1 pixel của thủy vân
+            b = bitget(watermarked_array(p(index)),1);  %Lấy ra bit ở vị trí thứ 1 (LSB) của pixel j của ảnh chứa
             if(b == 1)                                  %Nếu bit đó = 1
                 im_log(k) = bitset(im_log(k),j);        %Set giá trị cho bit j của pixel k của thủy vân là 1 (ngược lại là 0)
             end
         end
     end
-
+    
     im_log = uint8(im_log);
     varargout{1} = reshape(im_log,watermark_size(1),watermark_size(2),watermark_size(3));
 
